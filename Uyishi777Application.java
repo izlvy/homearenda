c66f3bde6bfb3ca1b3d9dd867a17092886192b4e
izlvy <asad717izz@gmail.com>
2025-05-21 09:46:14 +0500

    .

---
+++ Uyishi777Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uyishi777Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uyishi777Application.class, args);
+
+
+    }
+}
